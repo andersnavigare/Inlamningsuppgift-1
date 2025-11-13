00cc71c2bb4277368c50922fb834081f38f91f60
anders_navigare <anders.emanuel.tone@gmail.com>
2025-11-13 11:24:58 +0100

    Mitt fjärde commit

--- src/ovningarVecka6/SwitchCalculator.java
+++ src/ovningarVecka6/SwitchCalculator.java
@@ -1,61 +1,54 @@
 package ovningarVecka6;
 
-import java.util.Scanner;
-
 public class SwitchCalculator {
 
-    public static void main(String[] args) {
+        int x;
+        int y;
+        static char operation;
 
-        Scanner scan = new Scanner(System.in);
 
-        System.out.println("Välj 1: Addition");
-        System.out.println("Välj 2: Subtraktion");
-        System.out.println("Välj 3: Multiplikation");
-        System.out.println("Välj 4: Division");
+        public void setX(int i) {
+            this.x = i;
+        }
 
-        int choice = Integer.parseInt(scan.nextLine());
-        int tal1;
-        int tal2;
+        public void setY(int i) {
+            this.y = i;
+        }
 
-        switch (choice) {
-            case 1:
-                System.out.println("Addition: Skriv in ditt första nummer:");
-                tal1 = Integer.parseInt(scan.nextLine());
-                System.out.println("Skriv in ditt andra nummer:");
-                tal2 = Integer.parseInt(scan.nextLine());
-                System.out.println("Summan av dessa blev: " + (tal1 + tal2));
-                break;
-
-            case 2:
-                System.out.println("Subtraktion: Skriv in ditt första nummer:");
-                tal1 = Integer.parseInt(scan.nextLine());
-                System.out.println("Skriv in ditt andra nummer:");
-                tal2 = Integer.parseInt(scan.nextLine());
-                System.out.println("Summan av dessa blev: " + (tal1 - tal2));
-                break;
-
-            case 3:
-                System.out.println("Multiplikation: Skriv in ditt första nummer:");
-                tal1 = Integer.parseInt(scan.nextLine());
-                System.out.println("Skriv in ditt andra nummer:");
-                tal2 = Integer.parseInt(scan.nextLine());
-                System.out.println("Summan av dessa blev: " + (tal1 * tal2));
-                break;
-
-            case 4:
-                System.out.println("Division: Skriv in ditt första nummer:");
-                tal1 = Integer.parseInt(scan.nextLine());
-                System.out.println("Skriv in ditt andra nummer:");
-                tal2 = Integer.parseInt(scan.nextLine());
-                System.out.println("Summan av dessa blev: " + (tal1 / tal2));
-                break;
-
+        public void setOperation(char c) {
+            this.operation = c;
 
         }
 
-    }
-}
+        public int result() {
+            switch (operation) {
+                case '+':
+                    return x + y;
+                case '-':
+                    return x - y;
+                case '*':
+                    return x * y;
+                case '/':
+                    return x / y;
+            }
+            return -1;
+        }
 
-// Skapa en enkel kalkylator som tar emot två tal och en operation (addition, subtraktion, multiplikation, division)
-// från användaren. Använd en switch-sats för att bestämma vilken operation som ska utföras baserat på användarens input.
+        public static int resultStatic(int x, int y, char c) {
+
+            switch (operation) {
+                case '+':
+                    return x + y;
+                case '-':
+                    return x - y;
+                case '*':
+                    return x * y;
+                case '/':
+                    return x / y;
+            }
+            return -1;
+
+        }
+    }
+
 
